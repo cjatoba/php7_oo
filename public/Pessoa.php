9dfbee8112ddf2d25af1aec2f985520e74681741
cjatoba <claytonjatoba@gmail.com>
2021-10-16 20:07:20 -0300

    Aula 04 Atributos e métodos static, Método Destrutor

--- public/Pessoa.php
+++ public/Pessoa.php
@@ -4,12 +4,21 @@ class Pessoa{
 
     private string $nome;
     private int $idade;
+    static int $numeroDePessoas = 0;
 
     public function __construct(string $nome, int $idade)
     {
         $this->nome = $nome;
         $this->idade = $idade;
         $this->validaIdade($idade);
+        //o self é utilizado para fazer referencia a um atributo static
+        self::$numeroDePessoas++;
+    }
+
+    //Chamado quando se utiliza o unset em um objeto desta classe
+    public function __destruct()
+    {
+        self::$numeroDePessoas--;
     }
 
     public function getNome()
@@ -30,6 +39,10 @@ class Pessoa{
     public function setIdade(int $idade)
     {
         $this->idade = $idade;
+    }
+
+    public static function getNumeroDePessoas() {
+        return self::$numeroDePessoas;
     }
 
     private function validaIdade(int $idade)
--- public/index.php
+++ public/index.php
@@ -5,19 +5,7 @@ require_once 'Pessoa.php';
 $pessoa1 = new Pessoa('Nome da pessoa 1', 30);
 $pessoa2 = new Pessoa('Nome da pessoa 2', 40);
 
-$pessoa1->setNome('Novo nome da pessoa 1');
-$pessoa1->setIdade(50);
+//Destroi o a instância da classe Pesso e chama o método __destruct na classe Pessoa
+unset($pessoa2);
 
-$pessoa2->setNome('Novo nome da pessoa 2');
-$pessoa2->setIdade(70);
-
-echo "<p>Nome: {$pessoa1->getNome()} </p>";
-echo "<p>Idade: {$pessoa1->getIdade()} </p>";
-
-echo "<p>Nome: {$pessoa2->getNome()} </p>";
-echo "<p>Idade: {$pessoa2->getIdade()} </p>";
-
-echo '<pre>';
-    var_dump($pessoa1);
-    var_dump($pessoa2);
-echo '</pre>';
+echo '<p>Número de pessoas:' . Pessoa::getNumeroDePessoas() . '</p>';
