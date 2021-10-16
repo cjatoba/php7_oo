7a56b5247b74b59b1777c4a744478a95f757abd8
cjatoba <claytonjatoba@gmail.com>
2021-10-16 19:38:29 -0300

    Aula 02 Método Construtor, Métodos Get e Set

--- public/Pessoa.php
+++ public/Pessoa.php
@@ -5,4 +5,30 @@ class Pessoa{
     public string $nome;
     public int $idade;
 
+    public function __construct(string $nome, int $idade)
+    {
+        $this->nome = $nome;
+        $this->idade = $idade;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getIdade()
+    {
+        return $this->nome;
+    }
+
+    public function setIdade(int $idade)
+    {
+        $this->idade = $idade;
+    }
+
 }
--- public/index.php
+++ public/index.php
@@ -2,19 +2,20 @@
 
 require_once 'Pessoa.php';
 
-$pessoa1 = new Pessoa();
-$pessoa2 = new Pessoa();
+$pessoa1 = new Pessoa('Nome da pessoa 1', 30);
+$pessoa2 = new Pessoa('Nome da pessoa 2', 40);
 
-$pessoa1->nome = "Nome da pessoa 1";
-$pessoa1->idade = 30;
-$pessoa2->nome = "Nome da pessoa 2";
-$pessoa2->idade = 40;
+$pessoa1->setNome('Novo nome da pessoa 1');
+$pessoa1->setIdade(50);
 
-echo "<p>Nome: $pessoa1->nome </p>";
-echo "<p>Idade: $pessoa1->idade </p>";
-echo '<hr>';
-echo "<p>Nome: $pessoa2->nome </p>";
-echo "<p>Idade: $pessoa2->idade </p>";
+$pessoa2->setNome('Novo nome da pessoa 2');
+$pessoa2->setIdade(70);
+
+echo "<p>Nome: {$pessoa1->getNome()} </p>";
+echo "<p>Idade: {$pessoa1->getIdade()} </p>";
+
+echo "<p>Nome: {$pessoa2->getNome()} </p>";
+echo "<p>Idade: {$pessoa2->getIdade()} </p>";
 
 echo '<pre>';
     var_dump($pessoa1);
