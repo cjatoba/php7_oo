b663cc6cdc9207fce8379ae578d6e79d75ad6d6b
cjatoba <claytonjatoba@gmail.com>
2021-10-16 19:49:19 -0300

    Aula 03 Encapsulamento, private, Métodos Especiais ou Específicos

--- public/Pessoa.php
+++ public/Pessoa.php
@@ -2,13 +2,14 @@
 
 class Pessoa{
 
-    public string $nome;
-    public int $idade;
+    private string $nome;
+    private int $idade;
 
     public function __construct(string $nome, int $idade)
     {
         $this->nome = $nome;
         $this->idade = $idade;
+        $this->validaIdade($idade);
     }
 
     public function getNome()
@@ -29,6 +30,16 @@ class Pessoa{
     public function setIdade(int $idade)
     {
         $this->idade = $idade;
+    }
+
+    private function validaIdade(int $idade)
+    {
+        if ($this->idade > 0 AND $this->idade < 120) {
+            $this->idade = $idade;
+        } else {
+            echo 'Idade não permitida!';
+            exit;
+        }
     }
 
 }
